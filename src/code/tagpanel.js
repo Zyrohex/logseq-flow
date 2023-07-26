4efea8d9a164411c3ecef09ab62e569ebfd70d3a
Nick Martin <nicholas_martin@dell.com>
2023-07-26 08:06:09 -0500

    update

---
+++ src/code/tagpanel.js
@@ -0,0 +1,15 @@
+// Setup our static elements
+let newElement = document.createElement("div").classList.add("tagRefPanel")
+let newContent = document.createElement("div").classList.add("content")
+let titleContent = document.createElement("div").classList.add("foldable-title")
+
+// Create our event handler
+function buildTagPanel(elements) {
+    // create our panel
+
+    // add our title and collapse functions
+
+    // process our tagged elements and move them to our new panel
+}
+
+// Create our event listener
